6299bb0a6f9141017819fbaf6821037afaff8a78
prbronze <bronze_santos@hotmail.com>
2024-10-09 15:15:12 -0300

    Update streamlit_app.py
    add SEARCH_ON column when slicing DF

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ st.write("The name on your Smmothie will be:", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 
 # Convert the Snowpark Dataframe to a Pnadas Dataframe so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
